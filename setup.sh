a06134e0852519d9935956e51d556e586d8ee60b
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2019-08-05 20:43:27 +0900

    :sparkles: Add install process of prezto.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle --file=Brewfile
 
+# Install prezto for zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
 # Link any dotfiles
 for f in .??*
 do
